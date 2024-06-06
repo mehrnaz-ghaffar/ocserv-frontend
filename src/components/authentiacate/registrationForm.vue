33023356cf71aa828266a45d0803a8a91a815ee1
mehrnaz <mehrnaz.ghaffar@gmail.com>
2024-06-06 21:17:26 +0330

    added prod.env vars

---
+++ src/components/authentiacate/registrationForm.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import Vue from 'vue'
+
+defineProps<{
+//   msg: string
+}>()
+
+</script>
+
+
+<template>
+  <div class="text-xl bg-blue underline ">
+    hi
+  </div>
+</template>
