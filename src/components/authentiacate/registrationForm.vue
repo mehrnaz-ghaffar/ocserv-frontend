5bb23474eb91945fe63f4c36319197725958ce09
mehrnaz <mehrnaz.ghaffar@gmail.com>
2024-06-07 19:31:03 +0330

    test: how to add custom tailwind classes

--- src/components/authentiacate/registrationForm.vue
+++ src/components/authentiacate/registrationForm.vue
@@ -1,15 +1,13 @@
 <script setup lang="ts">
-import Vue from 'vue'
+import Vue from "vue";
 
 defineProps<{
-//   msg: string
-}>()
-
+  //   msg: string
+}>();
 </script>
 
-
 <template>
-  <div class="text-xl bg-blue underline ">
-    hi
+  <div class="text-xl bg-blue underline">
+    <input type="text" class="input m-5" />
   </div>
 </template>
