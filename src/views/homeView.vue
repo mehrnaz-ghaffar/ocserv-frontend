561ec55d54ac7796e4b84e1a61465b368966f62a
mehrnaz <mehrnaz.ghaffar@gmail.com>
2024-06-08 23:20:39 +0330

    added homeView

---
+++ src/views/homeView.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { RouterView, useRoute, useRouter } from "vue-router";
+import { useQuery, gql } from "@apollo/client";
+
+const GET_USERS = gql`
+  query {
+    users {
+      name
+      email
+    }
+  }
+`;
+
+// const UsersList = () => {
+//   const { loading, error, data } = useQuery(GET_USERS);
+
+//   if (loading) return <p>Loading...</p>;
+//   if (error) return <p>Error: {error.message}</p>;
+
+//   return (
+//     <ul>
+//       {data.users.map(user => (
+//         <li key={user.email}>{user.name} - {user.email}</li>
+//       ))}
+//     </ul>
+//   );
+// };
+</script>
