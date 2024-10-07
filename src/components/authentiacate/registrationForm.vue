e16fe5bf647add189ac9a8220d53303d0f099dc4
ghaffar <ghaffar@citynet.ir>
2024-10-07 15:22:16 +0330

    chore:added OStepper

--- src/components/authentiacate/registrationForm.vue
+++ src/components/authentiacate/registrationForm.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import OInput from "../UI/OInput.vue";
+import Ostepper from "../UI/OStepper.vue";
 
 defineProps<{
   //   msg: string
@@ -10,5 +11,7 @@ defineProps<{
   <div class="text-xl bg-blue underline">
     <input type="text" class="input m-5" />
     <OInput />
+
+    <Ostepper />
   </div>
 </template>
