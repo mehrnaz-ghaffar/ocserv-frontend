8305afc88ac5fae278905b51b0df6d2ceb571f7c
mehrnaz <ghaffar@citynet.ir>
2025-01-18 17:10:41 +0330

    feat: added socket io

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { QueryClientProvider } from "@tanstack/react-query";
+import socket from "./socket.ts";
 import queryClient from "./queryClient";
 import "./index.css";
 import App from "./App.tsx";
+
+socket.on("connect", () => {
+  console.log("Connected to the server:", socket.id);
+});
+
+socket.emit("message", "Hello from the client!");
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
